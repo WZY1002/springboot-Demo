6e4ef72846afb124a9d85903684f5d1d6675d6d4
吴智勇 <1418487237@qq.com>
2018-09-07 17:31:43 +0800

    socketRpc完善

--- src/main/test/demoTest.java
+++ src/main/test/demoTest.java
@@ -1,13 +1,18 @@
 import Http.HttpClintPost;
 import Http.HttpPost;
 import Http.Httpspost;
+import RPC.my_socket_rpc.model.UserVO;
 import impldemo.MainApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import sun.swing.BakedArrayList;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MainApplication.class)
@@ -34,8 +39,37 @@ public class demoTest {
         System.out.println(bb.setScale(2, BigDecimal.ROUND_HALF_DOWN));
         System.out.println(cc.setScale(2, BigDecimal.ROUND_HALF_UP));
         System.out.println(cc.setScale(2, BigDecimal.ROUND_HALF_DOWN));
-
     }
 
+    @Test
+    public void demo1(){
+        int a=1;
+        int b=1;
+        List<Integer> list=new ArrayList<>();
+        list.add(5);
+        dodemo1(a,b,list);
+        UserVO userVO;
+        if(true){
+            userVO=(UserVO)dodemo1(0,0,new ArrayList<>());
+        }
+//        System.out.println(a+b);
+//        System.out.println(userVO);
+//        RestTemplate restTemplate=new RestTemplate();
+        String furl="asda//aaas//aa/saasa/";
+        if(furl.contains("//")){
+            furl=furl.replaceAll("(////)(.*?)(\\1)(.*?)", "/");
+        }
+        System.out.println(furl);
+    }
+
+    public Object dodemo1(int a, int b, List<Integer> list){
+//        list.add(5);
+//        a=a+1;
+        List<UserVO> list1=new ArrayList<>();
+        UserVO userVO=new UserVO("",0,"");
+        list1.add(userVO);
+        return  list1.get(0);
+//        return  a+b;
+    }
 
 }
